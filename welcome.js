789018072bc351af3930a292cb3646e6ffdeb82a
phantathoang <phantathoang@gmail.com>
2026-07-01 00:05:08 +0700

    feat: make welcome page bilingual and set english as default language

--- welcome.js
+++ welcome.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Localization
+  document.querySelectorAll('[data-i18n]').forEach(element => {
+    const message = chrome.i18n.getMessage(element.getAttribute('data-i18n'));
+    if (message) {
+      element.innerHTML = message;
+    }
+  });
+
   const btnClose = document.getElementById('btnClose');
   if (btnClose) {
     btnClose.addEventListener('click', (e) => {
